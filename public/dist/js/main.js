355b570855e96fd17c73286b299b63672a05417c
Maubry94 <matthieu.aubry94370@gmail.com>
2023-10-27 14:54:14 +0200

    update TP1

--- public/dist/js/main.js
+++ public/dist/js/main.js
@@ -1,15 +1,14 @@
 window.onload = () => {
-    document.querySelectorAll('[data-component]')
-    .forEach((element) => {
-        fetch('./src/components/' + element.getAttribute('data-component') + '.html')
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`erreur HTTP! statut: ${reponse.status}`);
-            }
-            return response.text();
-        })
-        .then((text) => {
-            element.innerHTML = text;
-        })
-    })
-}
+	document.querySelectorAll("[data-component]").forEach((element) => {
+		fetch("./src/html/" + element.getAttribute("data-component") + ".html")
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error(`erreur HTTP! statut: ${reponse.status}`);
+				}
+				return response.text();
+			})
+			.then((text) => {
+				element.innerHTML = text;
+			});
+	});
+};
